a6d17a472b7b2c4aff271c2d92a243cf24381f42
Shruti Phad <shrutiphadwork@gmail.com>
2026-02-05 15:51:27 +0530

    adding files

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <h1 className="text-4xl text-red-500 bg-black p-4">
+  Tailwind Working
+</h1>
+
+    </div>
+  );
+}
