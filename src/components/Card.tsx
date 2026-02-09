b60968bf2ef78bbef8744d428bf5e21d014dae0f
Shruti Phad <shrutiphadwork@gmail.com>
2026-02-09 23:43:50 +0530

    added the components

---
+++ src/components/Card.tsx
@@ -0,0 +1,22 @@
+import grainImage from "@/assets/images/grain.jpg";
+import { twMerge } from "tailwind-merge";
+
+export const Card = ({ className }: { className?: string }) => {
+  return (
+    <div
+      className={twMerge(
+        "bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:bg-purple-900 after:ring-8 after:ring-white/60 after:rounded-3xl p-6",
+        className
+      )}
+    >
+      <div
+        className="absolute inset-0 -z-10 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      />
+
+      {/* Your card content goes here */}
+    </div>
+  );
+};
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,20 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+  <div className="pb-96">
+      <SectionHeader
+        eyebrow="About Me"
+        title="A Glimpse Into My World"
+        description="Currently learning and leveling up."
+      />
+      <div>
+        <Card>
+
+        </Card>
+      </div>
+
+    </div>
+  )  
 };
